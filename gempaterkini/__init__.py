27ab4298ea63f91206d3116e87461955db55c648
adli138 <adlimuhammad847@gmail.com>
2024-07-02 11:12:57 +0700

    Add files via upload

---
+++ gempaterkini/__init__.py
@@ -0,0 +1,70 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+def ekstraksi_data():
+    try:
+        content = requests.get('https://www.bmkg.go.id/')
+    except Exception:
+        return None
+
+    if content.status_code == 200:
+        soup = BeautifulSoup(content.text, 'html.parser')
+
+        result = soup.find('span', {'class': 'waktu'})
+        result = result.text.split(', ')
+        tanggal = result[0]
+        waktu = result[1]
+
+        result = soup.find('div', {'class': 'col-md-6 col-xs-6 gempabumi-detail no-padding'})
+        result = result.findChildren('li')
+        i = 0
+        magnitudo = None
+        koordinat = None
+        kedalaman = None
+        lokasi = None
+        dirasakan = None
+
+        for res in result:
+            if i == 1:
+                magnitudo = res.text
+            elif i == 2:
+                kedalaman = res.text
+            elif i == 3:
+                koordinat = res.text
+            elif i == 4:
+                lokasi = res.text
+            elif i == 5:
+                dirasakan = res.text
+
+            i = i + 1
+
+        hasil = dict()
+        hasil['tanggal'] = tanggal
+        hasil['waktu'] = waktu
+        hasil['magnitudo'] = magnitudo
+        hasil['kedalaman'] = kedalaman
+        hasil['koordinat'] = koordinat
+        hasil['lokasi'] = lokasi
+        hasil['dirasakan'] = dirasakan
+        return hasil
+    else:
+        return None
+
+
+def tampilkan_data(result):
+    if result is None:
+        print('Tidak bisa menemukan data gempa terkini')
+        return
+    print('Gempa terakhir bedasarkan BMKG')
+    print(f"Tanggal : {result['tanggal']}")
+    print(f"Waktu : {result['waktu']}")
+    print(f"Magnitudo : {result['magnitudo']}")
+    print(f"Kedalaman : {result['kedalaman']}")
+    print(f"Koordinat : {result['koordinat']}")
+    print(f"Lokasi : {result['lokasi']}")
+    print(result['dirasakan'])
+
+if __name__ == '__main__':
+    result = ekstraksi_data()
+    tampilkan_data(result)
